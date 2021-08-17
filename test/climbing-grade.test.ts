8571a545701c824f60a9199c05d59acd709ccf59
JanBloch <59955205+JanBloch@users.noreply.github.com>
2021-08-17 13:51:39 +0200

    Update climbing-grade.test.ts
    fix test ( incorrect conversion)

--- test/climbing-grade.test.ts
+++ test/climbing-grade.test.ts
@@ -2,7 +2,7 @@ import ClimbingGrade from "../src/climbing-grade";
 
 test("when given a system, ClimbingGrade.format should return the grade in specified system", () => {
   const grade = new ClimbingGrade("5a", "french");
-  expect(grade.format("yds")).toBe("5.8");
+  expect(grade.format("yds")).toBe("5.7");
 });
 
 test("if a grade matches multiple grades in another system, a range is returned", () => {
