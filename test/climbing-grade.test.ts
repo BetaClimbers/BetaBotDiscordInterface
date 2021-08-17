aecdb8d627aaf55157156ef556c0afa336201015
JanBloch <jan.bloch@noseryoung.ch>
2021-08-17 13:06:53 +0200

    add example tests

---
+++ test/climbing-grade.test.ts
@@ -0,0 +1,11 @@
+import ClimbingGrade from "../src/climbing-grade";
+
+test("when given a system, ClimbingGrade.format should return the grade in specified system", () => {
+  const grade = new ClimbingGrade("5a", "french");
+  expect(grade.format("yds")).toBe("5.8");
+});
+
+test("if a grade matches multiple grades in another system, a range is returned", () => {
+  const grade = new ClimbingGrade("7a", "french", { rangeDelimiter: "/" });
+  expect(grade.format("yds")).toBe("5.11c/5.11d");
+});
---
+++ test/commands/credits.test.ts
@@ -0,0 +1,13 @@
+import { Credits } from "../../src/commands/Credits";
+
+const mockReply = jest.fn();
+const MockCommandInteraction = jest.fn().mockImplementation(() => {
+  return { reply: mockReply };
+});
+class C extends Credits {}
+
+test("when the command is executed, it will reply to the interaction", () => {
+  const c = new C();
+  c.credits(new MockCommandInteraction());
+  expect(mockReply).toHaveBeenCalledTimes(1);
+});
